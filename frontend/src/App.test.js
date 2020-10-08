96051ae42c00f14aae8fe87ec58b33209537954d
xkylesun <xyasun@gmail.com>
2020-10-08 16:53:57 -0700

    new

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import { screen } from '@testing-library/dom'
 
-test('renders learn react link', () => {
+test('properly display startup page', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Miles Challenge/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+
+// test ("can add reward", () => {
+//   const { getByText } = render(<App />);
+// });
+
+
+// test("can add reward", () => {
+
+// });
+
+
+// test('uses jest-dom', () => {
+//   document.body.innerHTML = `
+//     <span data-testid="not-empty"><span data-testid="empty"></span></span>
+//     <div data-testid="visible">Visible Example</div>
+//   `
+
+//   expect(screen.queryByTestId('not-empty')).not.toBeEmptyDOMElement()
+//   expect(screen.getByText('Visible Example')).toBeVisible()
+// })
