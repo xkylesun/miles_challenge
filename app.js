359918d132eb3303d5c8b4555a048e1a7d33c0d4
xkylesun <xyasun@gmail.com>
2020-10-08 17:27:59 -0700

    test

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-const port = process.env.PORT || 9000;
-// app.listen(port, () => console.log(`Server is running on port ${port}`));
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
 
-app.use(express.static(path.join(__dirname, 'frontend/build')));
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-app.listen(port);
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+}
